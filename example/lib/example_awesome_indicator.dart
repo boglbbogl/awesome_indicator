d47166bb389f791460b1e6dd2af3e1e1c48424a5
boglbbogl <75574246+boglbbogl@users.noreply.github.com>
2023-12-04 19:28:44 +0900

    creatre example

---
+++ example/lib/example_awesome_indicator.dart
@@ -0,0 +1,122 @@
+import 'package:awesome_indicator/awesome_indicator.dart';
+import 'package:flutter/material.dart';
+
+class ExampleAwesomeIndicator extends StatefulWidget {
+  const ExampleAwesomeIndicator({super.key});
+
+  @override
+  State<ExampleAwesomeIndicator> createState() =>
+      _ExampleAwesomeIndicatorState();
+}
+
+class _ExampleAwesomeIndicatorState extends State<ExampleAwesomeIndicator> {
+  final ScrollController caseOneController = ScrollController();
+
+  double caseOnePosition = 0;
+  int caseOneRatio = 0;
+
+  final Color background = const Color.fromRGBO(26, 26, 26, 1);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: background,
+      appBar: AppBar(
+        title: const Text(
+          "Slider Indicator",
+          style: TextStyle(
+              fontWeight: FontWeight.bold, fontSize: 28, color: Colors.white),
+        ),
+        backgroundColor: background,
+      ),
+      body: ListView(
+        children: [
+          Container(
+            margin: const EdgeInsets.only(top: 24),
+            height: 200,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  height: 50,
+                  color: Colors.transparent,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        "Case 1",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.white),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Position : ${caseOnePosition.toStringAsFixed(2).toString()}",
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 12,
+                                color: Color.fromRGBO(155, 155, 155, 1)),
+                          ),
+                          Text(
+                            "Ratio : ${caseOneRatio.toString()}",
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 12,
+                                color: Color.fromRGBO(155, 155, 155, 1)),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                    height: 150,
+                    child: SingleChildScrollView(
+                      controller: caseOneController,
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: [
+                          const SizedBox(width: 20),
+                          ...List.generate(
+                            100,
+                            (index) => Container(
+                              width: 150,
+                              height: 150,
+                              margin: const EdgeInsets.only(right: 20),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(8),
+                                color:
+                                    Colors.accents[index % 15].withOpacity(0.8),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    )),
+              ],
+            ),
+          ),
+          const SizedBox(height: 24),
+          AwesomeIndicator(
+            width: MediaQuery.of(context).size.width - 40,
+            controller: caseOneController,
+            color: Colors.white,
+            indicator: 0.35,
+            backgroud: const Color.fromRGBO(95, 95, 95, 1),
+            onListener: (int ratio, double position) {
+              setState(() {
+                caseOnePosition = position;
+                caseOneRatio = ratio;
+              });
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/awesome_indicator.dart
+++ lib/awesome_indicator.dart
@@ -1,7 +1,120 @@
 library awesome_indicator;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class AwesomeIndicator extends StatefulWidget {
+  final ScrollController controller;
+  final double? height;
+  final double? width;
+  final double indicator;
+  final Color? backgroud;
+  final Color? color;
+  final double? radius;
+  final Border? border;
+  final Function(int, double)? onListener;
+
+  const AwesomeIndicator({
+    super.key,
+    required this.controller,
+    this.height,
+    this.width,
+    this.backgroud,
+    this.color,
+    this.radius,
+    this.indicator = 0.5,
+    this.border,
+    this.onListener,
+  });
+
+  @override
+  State<AwesomeIndicator> createState() => _AwesomeIndicatorState();
+}
+
+class _AwesomeIndicatorState extends State<AwesomeIndicator> {
+  double _height = 0;
+  double _width = 0;
+  double _indicator = 0;
+
+  final ValueNotifier<double> _position = ValueNotifier(0);
+
+  @override
+  void didUpdateWidget(covariant AwesomeIndicator oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    _height = widget.height ?? 8;
+    _width = widget.width ?? MediaQuery.of(context).size.width;
+    _setIndicator();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _height = widget.height ?? 8;
+    widget.controller.addListener(() => _listener());
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _width = widget.width ?? MediaQuery.of(context).size.width;
+    _setIndicator();
+  }
+
+  void _setIndicator() {
+    _indicator = widget.indicator >= 1
+        ? 1
+        : widget.indicator <= 0
+            ? 0
+            : widget.indicator;
+  }
+
+  void _listener() {
+    double box = _width * _indicator;
+    double ratio = widget.controller.position.pixels /
+        widget.controller.position.maxScrollExtent;
+    _position.value = (_width - box) * ratio;
+    if (widget.onListener != null) {
+      widget.onListener!((ratio * 100).round(), _position.value);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          width: _width,
+          height: _height,
+          decoration: BoxDecoration(
+            border: widget.border,
+            borderRadius: BorderRadius.circular(widget.radius ?? _width),
+            color: widget.backgroud ?? const Color.fromRGBO(195, 195, 195, 1),
+          ),
+          child: Stack(
+            children: [
+              ValueListenableBuilder<double>(
+                  valueListenable: _position,
+                  builder: (
+                    BuildContext context,
+                    double position,
+                    Widget? child,
+                  ) {
+                    return Positioned(
+                      left: position,
+                      child: Container(
+                        width: _width * _indicator,
+                        height: _height,
+                        decoration: BoxDecoration(
+                          borderRadius:
+                              BorderRadius.circular(widget.radius ?? _width),
+                          color: widget.color ?? Colors.black,
+                        ),
+                      ),
+                    );
+                  }),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
 }
